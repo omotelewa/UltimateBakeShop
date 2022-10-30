1e364e4bd696767907a5557291c2ab31d99ebae8
Marian Rahman <rahman.marian@gmail.com>
2022-10-30 10:40:02 -0400

    Updated class name and added mySQL dependency

--- src/main/java/com/ultimatebake/shop/TheUltimateBakeShopApplication.java
+++ src/main/java/com/ultimatebake/shop/TheUltimateBakeShopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TheUlitmateBakeShopApplication {
+public class TheUltimateBakeShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TheUlitmateBakeShopApplication.class, args);
+		SpringApplication.run(TheUltimateBakeShopApplication.class, args);
 	}
 
 }
